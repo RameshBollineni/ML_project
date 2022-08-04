b65d8aae1c84e9aae3dd1c8f83643fb6f574a8e8
Ramesh Bollineni <bollineniramesh.informatica@gamil.com>
2022-08-05 00:55:02 +0530

    logger & Expection

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import sys 
+from creditcard.logger import logging
+from creditcard.expection import CreditcardExpection
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])  
 
 def index():
+    try:
+        raise Exception("We are testing custom expetcion")
+    except Exception as e: 
+        creditcard = CreditcardExpection(e,sys)
+        logging.info(creditcard.error_message)
+        logging.info('We are testing logging module')
+
     return "This is my first project in ML"
+
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ creditcard/logger/__init__.py
@@ -0,0 +1,19 @@
+import logging 
+from datetime import datetime
+import os
+
+LOG_FOLDER="creditcard_logs"
+
+CURRENT_TIME_STAMP= f"{ datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+
+LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
+
+os.makedirs(LOG_FOLDER,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(LOG_FOLDER,LOG_FILE_NAME)
+
+logging.basicConfig(filename=LOG_FILE_PATH,
+filemode="w",
+format='[%(asctime)s] %(name)s -%(levelname)s -%(message)s',
+level=logging.INFO
+)
