e12309eadb46c7d0356ad34419bd19c17b3bea9d
Ramesh Bollineni <bollineniramesh.informatica@gamil.com>
2022-08-04 20:30:59 +0530

    dockerfile added

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
-#declaring variables for setup 
-PROJECT_NAME='creaditcard',
-VERSION='0.0.1',
-AUTHOR='ramesh',
-DESCRIPTION='THIS IS MY FIRST ML PROJECT',
-PACKAGES=['creaditcard']
+#Declaring variables for setup functions
+PROJECT_NAME="creadt_card_default_orediction"
+VERSION="0.0.1"
+AUTHOR="Ramesh Bollineni"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+PACKAGES=["creadtcard"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
-def get_requirements_list()->List[str]:
-    with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
 
-set(
-    name=PROJECT_NAME,
-    version=VERSION,
-    author=AUTHOR,
-    description=DESCRIPTION,
-    Packages=PACKAGES,
-    install_requires=get_requirements_list()
-    
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove('-e .')
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(),
+install_requires=get_requirements_list()
+
 )
