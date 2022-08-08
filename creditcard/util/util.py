50c7f0505aa3a4fdab8325b3340a2a762259bf96
Ramesh Bollineni <bollineniramesh.informatica@gamil.com>
2022-08-09 00:23:33 +0530

    adding config

---
+++ creditcard/util/util.py
@@ -0,0 +1,15 @@
+import yaml
+from creditcard.expection import CreditcardExpection
+import os,sys 
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise CreditcardExpection(e,sys) from e
+
